6e327bb5444ef5fade5ccc7fa095284315bafced
Fluedper <fluedper@163.com>
2018-03-26 14:40:31 +0800

    成功爬取第一页

---
+++ day-7/cateye_top100.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_one_page(url):
+	header = {
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+	}
+	response = requests.get(url,headers=header)
+	if response.status_code == 200:
+		return response.text
+	return None
+
+def main():
+	url = "http://maoyan.com/board/4"
+	html = get_one_page(url)
+	print(html)
+main()
